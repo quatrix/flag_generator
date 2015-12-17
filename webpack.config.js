e02c562ce74609c8491aa4d14f856f01a665b582
quatrix <quatrix@Admins-MacBook-Pro.local>
2015-12-17 12:50:39 +0200

    reactifying

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 module.exports = {
-    entry: "./entry.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    }
-};
+  context: __dirname + "/app",
+
+  entry: {
+    javascript: "./app.js",
+    html: "./index.html",
+  },
+
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist",
+  },
+
+  module: {
+    loaders: [
+	  {
+	    test: /\.js$/,
+	    exclude: /node_modules/,
+	    loader: "babel",
+        query: {
+          presets: ['react', 'es2015']
+        }
+	  },
+	  {
+	    test: /\.html$/,
+	    loader: "file?name=[name].[ext]",
+	  },
+	]
+  }
+}
